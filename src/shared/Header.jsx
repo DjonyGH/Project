4ad1a8b82c614abf481facb3e7a1d1903d069695
DjonyGH <counsil@mail.ru>
2020-08-23 13:06:42 +0700

    added hmr

--- src/shared/Header.jsx
+++ src/shared/Header.jsx
@@ -1,10 +1,14 @@
+import { hot } from 'react-hot-loader/root'
 import React from 'react';
 
-export function Header() {
+function HeaderComponent() {
     return(
         <header>
             <h1>Our react project!!!</h1>
+            <p>Hello there</p>
         </header>
     );
 }
+
+export const Header = hot(HeaderComponent);
 
